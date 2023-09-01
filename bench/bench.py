1448a56bc9c08f5f483a63e1706660fe6ab15368
fescudie <escudie.frederic@iuct-oncopole.fr>
2023-09-01 17:02:34 +0200

    First import.

---
+++ bench/bench.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python3
+
+__author__ = 'Frederic Escudie'
+__copyright__ = 'Copyright (C) 2023 CHU Toulouse'
+__license__ = 'GNU General Public License'
+__version__ = '1.0.0'
+
+import argparse
+import csv
+import hashlib
+import json
+import logging
+import os
+import psutil
+import shutil
+import sys
+import subprocess
+import time
+
+
+########################################################################
+#
+# FUNCTIONS
+#
+########################################################################
+class LoggerAction(argparse.Action):
+    """Manages logger level parameters (The value "INFO" becomes logging.info and so on)."""
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        log_level = None
+        if values == "DEBUG":
+            log_level = logging.DEBUG
+        elif values == "INFO":
+            log_level = logging.INFO
+        elif values == "WARNING":
+            log_level = logging.WARNING
+        elif values == "ERROR":
+            log_level = logging.ERROR
+        elif values == "CRITICAL":
+            log_level = logging.CRITICAL
+        setattr(namespace, self.dest, log_level)
+
+
+def submitMonitoredProcess(cmd, wait=1):
+    start_time = time.time()
+    with open("error.txt", "w") as fh_err:
+        with open("log.txt", "w") as fh_stdout:
+            proc = subprocess.Popen(cmd, shell=True, stderr=fh_err, stdout=fh_stdout)
+        max_mem = getMaxExecMem(proc.pid, "rss", wait)
+        end_time = time.time()
+        if proc.poll() != 0:
+            raise Exception("Error in subprocess: {}".format(cmd))
+    return start_time, end_time, max_mem
+
+
+def getMaxExecMem(pid, tag="rss", wait=1):
+    max_mem = 0
+    check = False
+    proc = psutil.Process(pid)
+    while proc.is_running() and proc.status() != psutil.STATUS_ZOMBIE:
+        max_mem = max(max_mem, getattr(proc.memory_info(), tag))
+        check = True
+        time.sleep(wait)
+    return max_mem if check else None
+
+
+def compress(cmd, input, output, metrics, retry=0):
+    complete_cmd = cmd.replace('#IN#', input).replace('#OUT#', output)
+    log.debug(complete_cmd)
+    is_ended = False
+    while not is_ended:
+        try:
+            start_time, end_time, metrics["compress_mem"] = submitMonitoredProcess(complete_cmd, 2)
+            is_ended = True
+            metrics["compress_time"] = end_time - start_time
+            metrics["compress_size"] = getFileSize(output)
+        except Exception:
+            retry -= 1
+            with open("error.txt") as reader:
+                metrics["compress_error"] = reader.readlines()
+            metrics["compress_nb_retries"] += 1
+            if retry < 0:
+                is_ended = True
+            log.warning(complete_cmd)
+
+
+def decompress(cmd, input, output, metrics, retry=0):
+    complete_cmd = cmd.replace('#IN#', input).replace('#OUT#', output)
+    log.debug(complete_cmd)
+    is_ended = False
+    while not is_ended:
+        try:
+            start_time, end_time, metrics["decompress_mem"] = submitMonitoredProcess(complete_cmd, 2)
+            is_ended = True
+            metrics["decompress_time"] = end_time - start_time
+            metrics["decompress_size"] = getFileSize(output)
+            metrics["decompress_hash"] = md5sum(output, chunk_size=8192)
+        except Exception:
+            retry -= 1
+            with open("error.txt") as reader:
+                metrics["decompress_error"] = reader.readlines()
+            metrics["decompress_nb_retries"] += 1
+            if retry < 0:
+                is_ended = True
+            log.warning(complete_cmd)
+
+
+def getFileSize(path):
+    file_stats = os.stat(path)
+    return file_stats.st_size
+
+
+def md5sum(path, chunk_size=8192):
+    """
+    Return MD5 checksum for the file.
+
+    :param path: Path to the file.
+    :type path: str
+    :param chunk_size: Size of chunks.
+    :type chunk_size: int
+    :return: MD5 checksum for the file.
+    :rtype: str
+    """
+    hashsum = hashlib.md5()
+    with open(path, "rb") as reader:
+        chunk = reader.read(chunk_size)
+        while chunk:  # while chunk := reader.read(chunk_size):
+            hashsum.update(chunk)
+            chunk = reader.read(chunk_size)
+    return hashsum.hexdigest()
+
+
+########################################################################
+#
+# MAIN
+#
+########################################################################
+if __name__ == "__main__":
+    # Manage parameters
+    parser = argparse.ArgumentParser(description='Generate metrics for benchmark of sequence compression algorithms.')
+    parser.add_argument('-l', '--logging-level', default="INFO", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], action=LoggerAction, help='The logger level. [Default: %(default)s]')
+    parser.add_argument('-w', '--working-dir', required=True, help='Path to working directory.')
+    parser.add_argument('-v', '--version', action='version', version=__version__)
+    group_input = parser.add_argument_group('Inputs')  # Inputs
+    group_input.add_argument('-a', '--algorithms', required=True, help='Path to configuration file on algorithms. (format: JSON).')
+    group_input.add_argument('-d', '--datasets', required=True, help='Path to configuration file on datasets. (format: JSON).')
+    group_output = parser.add_argument_group('Outputs')  # Outputs
+    group_output.add_argument('-m', '--metrics', required=True, help='Path to the metrics file. (format: CSV).')
+    args = parser.parse_args()
+
+    # Logger
+    logging.basicConfig(format='%(asctime)s -- [%(filename)s][pid:%(process)d][%(levelname)s] -- %(message)s')
+    log = logging.getLogger(os.path.basename(__file__))
+    log.setLevel(args.logging_level)
+    log.info("Command: " + " ".join(sys.argv))
+
+    # Load configurations
+    with open(args.algorithms) as reader:
+        algorithms = json.load(reader)
+    with open(args.datasets) as reader:
+        datasets = json.load(reader)
+
+    # Process
+    with open(args.metrics, "w") as fh:
+        writer = csv.DictWriter(
+            fh,
+            fieldnames=[
+                "dataset",
+                "laboratory", "instrument_type", "matrix", "design",
+                "file", "expected_hash", "decompress_hash", "soft",
+                "decompress_size", "decompress_time", "decompress_mem",
+                "compress_size", "compress_time", "compress_mem",
+                "decompress_cmd", "decompress_nb_retries", "decompress_error",
+                "compress_cmd", "compress_nb_retries", "compress_error"
+            ]
+        )
+        writer.writeheader()
+        for curr_dataset in datasets:
+            for file in curr_dataset["paths"]:
+                log.info("Process file {}.".format(file))
+                if not os.path.exists(args.working_dir):
+                    os.makedirs(args.working_dir)
+                tmp_file = os.path.join(args.working_dir, os.path.basename(file))
+                shutil.copyfile(file, tmp_file)
+                ini_path = tmp_file[:-3]
+                subprocess.check_call(["gzip", "-d", tmp_file])
+                expected_hash = md5sum(ini_path, chunk_size=8192)
+                for algo in algorithms:
+                    log.info("Porcess with soft {}.".format(algo["soft"]))
+                    algo_metrics = {
+                        "dataset": curr_dataset["name"],
+                        "design": curr_dataset["design"],
+                        "instrument_type": curr_dataset["instrument_type"],
+                        "laboratory": curr_dataset["laboratory"],
+                        "matrix": curr_dataset["matrix"],
+                        "file": file,
+                        "expected_hash": expected_hash,
+                        "soft": algo["soft"],
+                        "decompress_cmd": algo["decompress_cmd"],
+                        "decompress_nb_retries": 0,
+                        "decompress_size": None,
+                        "decompress_time": None,
+                        "decompress_mem": None,
+                        "decompress_error": None,
+                        "compress_cmd": algo["compress_cmd"],
+                        "compress_nb_retries": 0,
+                        "compress_size": None,
+                        "compress_time": None,
+                        "compress_mem": None,
+                        "compress_error": None,
+                        "decompress_hash": None
+                    }
+                    algo_dir = os.path.abspath(os.path.join(args.working_dir, algo["soft"]))
+                    os.makedirs(algo_dir)
+                    copy_path = os.path.join(algo_dir, "tmp_in.fastq")
+                    shutil.copyfile(ini_path, copy_path)
+                    compressed_path = os.path.join(algo_dir, "tmp_c")
+                    compress(algo["compress_cmd"], copy_path, compressed_path, algo_metrics)
+                    decompressed_path = os.path.join(algo_dir, "tmp_d")
+                    decompress(algo["decompress_cmd"], compressed_path, decompressed_path, algo_metrics)
+                    writer.writerow(algo_metrics)
+                    shutil.rmtree(algo_dir)
+                os.remove(ini_path)
+            fh.flush()
+            os.fsync(fh)
+    log.info("End of job")
---
+++ zfq.py
@@ -0,0 +1,341 @@
+#!/usr/bin/env python3
+
+__author__ = 'Frederic Escudie'
+__copyright__ = 'Copyright (C) 2023 CHU Toulouse'
+__license__ = 'GNU General Public License'
+__version__ = '1.0.0'
+
+import argparse
+import gzip
+import hashlib
+import json
+import logging
+import os
+import shutil
+import subprocess
+import sys
+import tarfile
+import uuid
+
+
+def compress(in_path, out_path, skip_check, log):
+    """
+    Compress FastQ file to zfq.
+
+    :param in_path: Path to raw file (format: FastQ). It can be gzipped.
+    :type in_path: str
+    :param out_path: Path to compressed file (format: zfq).
+    :type out_path: str
+    :param skip_check: Skip md5sum comparison between original file (before compression) and compressed file after uncompression. Check should be skipped only if the original file was gzipped with a compression level different of 9.'
+    :type skip_check: bool
+    :param log: Logger of the script.
+    :type log: logging.Logger
+    """
+    # Create tmp dir
+    dest_dir = os.path.dirname(out_path)
+    tmp_dir = os.path.join(dest_dir, str(uuid.uuid4()))
+    if not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+    # Process
+    try:
+        modif_time = os.path.getmtime(in_path)
+        # Split fastq components
+        log.info("Split FastQ file components")
+        with suitableOpen(in_path) as reader:
+            with open(os.path.join(tmp_dir, "head.txt"), "w") as writer_head:
+                with open(os.path.join(tmp_dir, "seq.fa"), "w") as writer_seq:
+                    with open(os.path.join(tmp_dir, "qual.txt"), "w") as writer_qual:
+                        writer_seq.write(">\n")
+                        rec_id = reader.readline()
+                        while rec_id:
+                            writer_head.write(rec_id[1:])
+                            writer_seq.write(reader.readline().strip())
+                            reader.readline()  # separator
+                            writer_qual.write(reader.readline())
+                            rec_id = reader.readline()
+        # Get info
+        log.info("Store input info")
+        metrics = subprocess.check_output(
+            ["wc", "-lm", os.path.join(tmp_dir, "qual.txt")]
+        )
+        nb_lines, nb_nt, file_name = metrics.strip().split()
+        with open(os.path.join(tmp_dir, "info.json"), "w") as writer_info:
+            writer_info.write(json.dumps({
+                "seq": int(nb_lines),
+                "nt": int(nb_nt),
+                "md5": md5sum(in_path),
+                "mtime": modif_time
+            }))
+        # Compress sequences
+        log.info("Zstd compress sequences")
+        subprocess.check_output([
+            "zstd",
+            "--no-progress",
+            "-18",
+            os.path.join(tmp_dir, "seq.fa")
+        ])
+        os.remove(os.path.join(tmp_dir, "seq.fa"))
+        # Compress qualities
+        log.info("Zstd compress qualities")
+        subprocess.check_output([
+            "zstd",
+            "--no-progress",
+            os.path.join(tmp_dir, "qual.txt")
+        ])
+        os.remove(os.path.join(tmp_dir, "qual.txt"))
+        # Compress headers
+        log.info("Zstd compress headers")
+        subprocess.check_output([
+            "zstd",
+            "--no-progress",
+            os.path.join(tmp_dir, "head.txt")
+        ])
+        os.remove(os.path.join(tmp_dir, "head.txt"))
+        # Create tar
+        log.info("Create archive")
+        with tarfile.open(out_path, mode="w") as archive:
+            # Headers
+            archive.add(
+                os.path.join(tmp_dir, "head.txt.zst"),
+                arcname="head.txt.zst",
+                recursive=False,
+            )
+            # Sequences
+            archive.add(
+                os.path.join(tmp_dir, "seq.fa.zst"),
+                arcname="seq.fa.zst",
+                recursive=False,
+            )
+            # Qualities
+            archive.add(
+                os.path.join(tmp_dir, "qual.txt.zst"),
+                arcname="qual.txt.zst",
+                recursive=False,
+            )
+            # Info
+            archive.add(
+                os.path.join(tmp_dir, "info.json"),
+                arcname="info.json",
+                recursive=False,
+            )
+        os.utime(out_path, (modif_time, modif_time))
+    finally:
+        shutil.rmtree(tmp_dir)
+    # Test archive consistency
+    if not skip_check:
+        log.info("Test file consistency after uncompressing")
+        test_file_path = out_path + ".testmd5"
+        uncompress(out_path, test_file_path, False, FakeLogger())
+        os.remove(test_file_path)
+
+
+class FakeLogger:
+    """Fake logger used to hide log outputs."""
+    def critical(self, msg):
+        pass
+
+    def debug(self, msg):
+        pass
+
+    def error(self, msg):
+        pass
+
+    def info(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+
+def info(in_path):
+    """
+    Display information from sequence file: number of sequences, number of nucleotids and original file md5sum.
+
+    :param in_path: Path to the compressed file (format: zfq).
+    :type in_path: str
+    """
+    with tarfile.open(in_path) as archive:
+        with archive.extractfile("info.json") as reader_info:
+            in_info = json.load(reader_info)
+    print(in_info)
+
+
+def isGzip(path):
+    """
+    Return True if the file is gziped.
+
+    :param path: Path to processed file.
+    :type path: str
+    :return: True if the file is gziped.
+    :rtype: bool
+    """
+    is_gzip = None
+    FH_input = gzip.open(path)
+    try:
+        FH_input.readline()
+        is_gzip = True
+    except Exception:
+        is_gzip = False
+    finally:
+        FH_input.close()
+    return is_gzip
+
+
+def md5sum(path, chunk_size=8192):
+    """
+    Return MD5 checksum for the uncompressed file.
+
+    :param path: Path to the file.
+    :type path: str
+    :param chunk_size: Size of chunks.
+    :type chunk_size: int
+    :return: MD5 checksum for the uncompressed file.
+    :rtype: str
+    """
+    hashsum = hashlib.md5()
+    open_fct = gzip.open if isGzip(path) else open
+    with open_fct(path, "rb") as reader:
+        chunk = reader.read(chunk_size)
+        while chunk:  # while chunk := reader.read(chunk_size):
+            hashsum.update(chunk)
+            chunk = reader.read(chunk_size)
+    return hashsum.hexdigest()
+
+
+def suitableOpen(path, mode="r"):
+    """
+    Return object file from suitable open (gzip.open or open) and mode on provided file.
+
+    :param path: Path to the file.
+    :type path: str
+    :param mode: Open mode.
+    :type mode: str
+    :return: Object file from suitable open (gzip.open or open) and mode on provided file.
+    :rtype: _io.TextIOWrapper or _io.BufferedReader
+    """
+    if mode in {"a", "r"}:
+        if isGzip(path):
+            return gzip.open(path, mode + "t")
+        else:
+            return open(path, mode)
+    else:
+        if path.endswith(".gz"):
+            return gzip.open(path, mode + "t")
+        else:
+            return open(path, mode)
+
+
+def uncompress(in_path, out_path, skip_check, log):
+    """
+    Uncompress zfq file to FastQ.
+
+    :param in_path: Path to compressed file (format: zfq).
+    :type in_path: str
+    :param out_path: Path to raw file (format: Fastq). It can be gzipped if the file name endswith ".gz".
+    :type out_path: str
+    :param skip_check: Skip md5sum comparison between original file (before compression) and file after uncompression. Check should be skipped only if the original file was gzipped with a compression level different of 9.
+    :type skip_check: bool
+    :param log: Logger of the script.
+    :type log: logging.Logger
+    """
+    # Create tmp dir
+    dest_dir = os.path.dirname(out_path)
+    tmp_dir = os.path.join(dest_dir, str(uuid.uuid4()))
+    if not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+
+    # Process
+    try:
+        # Extract all
+        log.info("Extract archive in {}".format(tmp_dir))
+        with tarfile.open(in_path) as archive:
+            archive.extractall(tmp_dir)
+        # Get info
+        with open(os.path.join(tmp_dir, "info.json")) as reader_info:
+            in_info = json.load(reader_info)
+        # Uncompress sequences
+        log.info("Zstd decompress sequences")
+        subprocess.check_output([
+            "zstd",
+            "--no-progress",
+            "-d",
+            os.path.join(tmp_dir, "seq.fa.zst")
+        ])
+        os.remove(os.path.join(tmp_dir, "seq.fa.zst"))
+        # Uncompress qualities
+        log.info("Zstd decompress qualities")
+        subprocess.check_output([
+            "zstd",
+            "--no-progress",
+            "-d",
+            os.path.join(tmp_dir, "qual.txt.zst")
+        ])
+        os.remove(os.path.join(tmp_dir, "qual.txt.zst"))
+        # Uncompress headers
+        log.info("Zstd decompress IDs")
+        subprocess.check_output([
+            "zstd",
+            "--no-progress",
+            "-d",
+            os.path.join(tmp_dir, "head.txt.zst")
+        ])
+        os.remove(os.path.join(tmp_dir, "head.txt.zst"))
+        # Write FastQ
+        log.info("Write FastQ")
+        with suitableOpen(out_path, "w") as writer_fastq:
+            with open(os.path.join(tmp_dir, "head.txt")) as reader_head:
+                with open(os.path.join(tmp_dir, "seq.fa")) as reader_seq:
+                    with open(os.path.join(tmp_dir, "qual.txt")) as reader_qual:
+                        reader_seq.read(2)
+                        for rec_head, rec_qual in zip(reader_head, reader_qual):
+                            rec_len = len(rec_qual) - 1
+                            writer_fastq.write("@" + rec_head)
+                            writer_fastq.write(reader_seq.read(rec_len) + "\n")
+                            writer_fastq.write("+\n")
+                            writer_fastq.write(rec_qual)
+        os.utime(out_path, (in_info["mtime"], in_info["mtime"]))
+        # Test consistency
+        if not skip_check:
+            log.info("Check consistency from original file")
+            if in_info["md5"] != md5sum(out_path):
+                raise Exception("Uncompressed file from archive is not identical to original file.")
+    finally:
+        shutil.rmtree(tmp_dir)
+
+
+if __name__ == "__main__":
+    # Manage parameters
+    parser = argparse.ArgumentParser(description='Compress or uncompress FastQ file.')
+    parser.add_argument('-v', '--version', action='version', version=__version__)
+    sub_parsers = parser.add_subparsers(dest="command")
+    compress_parser = sub_parsers.add_parser("compress", help="Compress FastQ file to zfq.")
+    compress_parser.add_argument('-i', '--input', required=True, help='Path to raw file (format: FastQ). It can be gzipped.')
+    compress_parser.add_argument('-o', '--output', required=True, help='Path to compressed file (format: zfq).')
+    compress_parser.add_argument('-r', '--remove', action='store_true', help='Remove input file after process.')
+    compress_parser.add_argument('-s', '--skip-check', action='store_true', help='Skip md5sum comparison between original file (before compression) and compressed file after uncompression.')
+    info_parser = sub_parsers.add_parser("info", help="Return information from sequence file: number of sequences, number of nucleotids and original file md5sum.")
+    info_parser.add_argument('-i', '--input', required=True, help='Path to compressed file (format: zfq).')
+    uncompress_parser = sub_parsers.add_parser("uncompress", help="Uncompress zfq file to FastQ.")
+    uncompress_parser.add_argument('-i', '--input', required=True, help='Path to compressed file (format: zfq).')
+    uncompress_parser.add_argument('-o', '--output', required=True, help='Path to raw file (format: Fastq). It can be gzipped if the file name endswith ".gz".')
+    uncompress_parser.add_argument('-r', '--remove', action='store_true', help='Remove input file after process.')
+    uncompress_parser.add_argument('-s', '--skip-check', action='store_true', help='Skip md5sum comparison between original file (before compression) and file after uncompress.')
+    args = parser.parse_args()
+
+    # Actions
+    if args.command == "info":
+        info(args.input)
+    else:
+        # Logger
+        logging.basicConfig(format='%(asctime)s -- [%(filename)s][pid:%(process)d][%(levelname)s] -- %(message)s')
+        log = logging.getLogger(os.path.basename(__file__))
+        log.setLevel(logging.INFO)
+        log.info("Command: " + " ".join(sys.argv))
+        # Process
+        if args.command == "compress":
+            compress(args.input, args.output, args.skip_check, log)
+        else:
+            uncompress(args.input, args.output, args.skip_check, log)
+        if args.remove:
+            os.remove(args.input)
+        log.info("End of job")
